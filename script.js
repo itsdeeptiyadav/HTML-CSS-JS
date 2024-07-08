5166d3fc373ac0d55abd2e6f25ffa79ba6d1e860
itsdeeptiyadav <deeptiy795@gmail.com>
2024-07-09 00:19:37 +0530

    ...

--- script.js
+++ script.js
@@ -6,6 +6,22 @@
 // console.log(3>5?'i win':'i did it');
 // let a=BigInt(9999999999999);
 // console.log(typeof(a));
-let a=`love
- babbar`;
-console.log(a);
+// let a=`love
+// babbar`;
+// console.log(a);
+// function saymyname(){
+//     console.log("love babbar")
+// }
+// saymyname();
+let obj={
+    name :"deep",
+    "full name": "deepti yadav",
+    weight:50,
+    age:20,
+    greet: function(){
+        console.log("hello");
+    }
+
+};
+console.log(obj);
+obj.greet();

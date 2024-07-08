3f85ab2534214793f026efa4bc7b2968a5c6b117
itsdeeptiyadav <deeptiy795@gmail.com>
2024-07-08 15:01:55 +0530

    ..

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
-console.log("namaste dunia");
-let a=20;
-a='babar'
+// console.log("namaste dunia");
+// let a=20;
+
+// console.log(a++);
+// // console.log(a);
+// console.log(3>5?'i win':'i did it');
+// let a=BigInt(9999999999999);
+// console.log(typeof(a));
+let a=`love
+ babbar`;
 console.log(a);

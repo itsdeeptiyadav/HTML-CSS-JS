4f23ef8053adc3fe730ad6378c0e1d462f28f5f6
itsdeeptiyadav <deeptiy795@gmail.com>
2024-07-12 16:26:18 +0530

    ...

---
+++ event.js
@@ -0,0 +1,12 @@
+let paras = document.querySelectorAll('p');
+function change(){
+    alert("you clicked on para:");
+
+
+}
+
+for(let j=0;j<paras.length;j++){
+    let para=paras[j];
+    para.addEventListener('click',change());
+}
+
